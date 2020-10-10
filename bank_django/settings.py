5d85f07b0552fcba4c3efcf9d6f0604ba20a7804
sravanje <sravan.jayati@gmail.com>
2020-10-10 20:32:34 +0530

    fixing urls

--- bank_django/settings.py
+++ bank_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aw@u)7ii-bi)498^llh8j*bqfc(te7_%b%lj251yh@&$*6@jyv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'portugese-bank-app.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'portugese-bank-app.herokuapp.com']
 
 
 # Application definition
